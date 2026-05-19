4b8ccc95eb6a4ba439580e688f2cd6c744d54a1d
souvik <souvikde.tech@gmail.com>
2026-05-19 22:36:31 +0530

    feat: moved to moonrepo

--- apps/server/main.py
+++ apps/server/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/ping")
-def ping():
-    return {"status": "ok"}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
